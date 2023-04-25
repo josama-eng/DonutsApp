5deac06cf60ca4cdb32bfabeb57cdec4e2aa6b66
josama-eng <samardzicjovana27@gmail.com>
2023-04-25 21:29:47 +0200

    change url

--- frontend/src/components/payment/PaymentComponent.js
+++ frontend/src/components/payment/PaymentComponent.js
@@ -17,7 +17,7 @@ const PaymentComponent = ({ ck }) => {
     stripe.confirmPayment({
       elements,
       confirmParams: {
-        return_url: "https://whimsical-scone-36891d.netlify.app/order",
+        return_url: "https://timely-stardust-3f57ec.netlify.app/order",
       },
     });
   };
